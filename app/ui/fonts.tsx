49bb586d8c4d81dce7d31502242e6586f68c92df
Azaz Ahmad <94698852+AzazMirza@users.noreply.github.com>
2025-12-02 16:16:35 +0500

    Delete app directory

--- app/ui/fonts.tsx
+++
@@ -1,9 +0,0 @@
-// app/ui/fonts.ts
-import { Lusitana } from 'next/font/google';
-
-export const lusitana = Lusitana({
-  weight: ['400', '700'], // Specify the weights you need
-  subsets: ['latin'],     // Specify the character subsets you need (e.g., 'latin', 'latin-ext', 'cyrillic', etc.)
-  display: 'swap',        // Optional: Font display strategy (default is 'swap')
-  // You don't need to specify 'src' if you're using Google Fonts via next/font/google
-});
